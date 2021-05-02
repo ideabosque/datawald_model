ee39c20b4ca6fbdc9bb14a0c27310325bcf6a9e8
ideabosque <ideabosque@gmail.com>
2021-05-01 20:30:42 -0700

    Initial check in.

---
+++ datawald_model/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+__author__ = "bibow"
+
+__all__ = [
+    "itemreceipt_object_types",
+    "order_object_types",
+    "models",
+]
+from .itemreceipt_object_types import *
+from .order_object_types import *
+from .models import *
